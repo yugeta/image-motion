c28b4508857dc6992a00e6375e7d865a6c0e4772
Yugeta Koji <yugeta@myntinc.com>
2022-07-31 20:19:34 +0900

    issue #1 : bug-fix

--- player/options.js
+++ player/options.js
@@ -1,5 +1,5 @@
 export const Options = {
   service_name : 'image-motion',
   animation_duration : 0.05, // shapeアニメーションの処理タイミング(s)
-  roots : {}
+  shapes : {}
 }
--- sample/js/play.js
+++ sample/js/play.js
@@ -3,6 +3,7 @@ import { Player } from '../../player/player.js'
 // データからアニメーション設定の初期化
 const chara_sample = new Player({
   file     : './sample5-d.json',  // データファイルのパス（読み込み元ファイル[*html]からの相対パス）
+  // file     : '../../pepa-web/assets/images/characters/pepa.json',  // データファイルのパス（読み込み元ファイル[*html]からの相対パス）
   selector : '#chara_sample',     // キャラクター画像をloadする要素の指定
   // name     : 'sample',            // キャラクター名(必須 : ユニーク値)
 })
