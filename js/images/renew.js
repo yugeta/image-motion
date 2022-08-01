a8664ad1497cd5774bd951998678b4ce20f52bfc
Yugeta Koji <yugeta@myntinc.com>
2022-08-01 09:53:30 +0900

    add-function : image replace

--- js/images/renew.js
+++ js/images/renew.js
@@ -1,25 +1,34 @@
 import { Options }      from '../options.js'
-import { Images  }      from './images.js'
 
-export class Upload{
+export class Renew{
   constructor(){
+    const uuid = this.get_active_uuid()
+    if(!uuid){return}
+    this.uuid = uuid
     this.click()
   }
   click(){
     let input_file = document.createElement("input")
     input_file.type     = 'file'
-    input_file.multiple = 'multiple'
+    // input_file.multiple = 'multiple'
     // input_file.name     = 'images[]'
     input_file.accept   = 'image/*'
     input_file.addEventListener('change' , this.upload_files.bind(this))
     document.querySelector("form[name='upload']").appendChild(input_file)
     input_file.click()
   }
+
   upload_files(e){
     if(!e.target.files.length){return}
-    for(let file of e.target.files){
-      new Images({file:file})
+    if(!this.uuid){return}
+    const data = e.target.files[0]
+    if(Options.img_datas[this.uuid]){
+      Options.img_datas[this.uuid].renew(data)
     }
   }
-  
+
+  get_active_uuid(){
+    const elm = Options.elements.get_active_view()
+    return elm ? elm.getAttribute('data-uuid') : null
+  }
 }
